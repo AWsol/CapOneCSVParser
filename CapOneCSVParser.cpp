b6c461ee8ed5da933bc1fbe3bda33964f054ec04
Andrzej <wsolandrzej@gmail.com>
2022-04-12 22:52:12 -0500

    Add files via upload
    Created a class to make it cleaner

--- CapOneCSVParser.cpp
+++ CapOneCSVParser.cpp
@@ -1,6 +1,7 @@
 // CapOneCSVParser.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "CapOneCSVReader.h"
 #include <fstream>
 #include <istream>
 #include <sstream>
@@ -10,9 +11,23 @@
 #include <vector>
 
 using namespace std;
-
+/*
+const static string AIRFARE = "Airfare";
+const static string CAR_RENTAL = "Car Rental";
+const static string DINING = "Dining";
+const static string ENTERTAINMENT = "Entertainment";
+const static string FEE_INT_CHARGE = "Fee/Interest Charge";
+const static string GAS_AUTOMOTIVE = "Gas/Automotive";
+const static string HEALTHCARE = "Health Care";
+const static string MERCHANDISE = "Merchandise";
+const static string OTHER = "Other";
+const static string OTHER_SERVICES = "Other Services";
+const static string OTHER_TRAVEL = "Other Travel";
+const static string PAYMENT_CREDIT = "Payment/Credit";
+const static string UTILITIES = "Utilities";
+*/
 int main() {
-
+    /*
     ifstream in("CapOneData2019.csv");
     if (!in.is_open())
         cout << "ERROR: File Open" << '\n';
@@ -37,14 +52,116 @@ int main() {
     for (int i = 0; i < dataVec.size(); i++)
         myTupleVec.push_back(make_tuple(dataVec[i][0], stod(dataVec[i][1])));
 
+    double sumAirfare = 0;
+    double sumCarRental = 0;
+    double sumDining = 0;
+    double sumEntertainment = 0;
+    double sumFeeIntCharge = 0;
+    double sumGasAutomotive = 0;
+    double sumHealthcare = 0;
+    double sumMerchandise = 0;
+    double sumOther = 0;
+    double sumOtherServices = 0;
+    double sumOtherTravel = 0;
+    double sumPaymentCredit = 0;
+    double sumUtilities = 0;
+    double sumInvalid = 0;
+
+    for (int i = 0; i < myTupleVec.size(); i++)
+    {
+        string category = get<0>(myTupleVec[i]);
+
+        if(category == AIRFARE)
+            sumAirfare += get<1>(myTupleVec[i]);
+        else if(category == CAR_RENTAL)
+            sumCarRental += get<1>(myTupleVec[i]);
+        else if (category == DINING)
+            sumDining += get<1>(myTupleVec[i]);
+        else if (category == ENTERTAINMENT)
+            sumEntertainment += get<1>(myTupleVec[i]);
+        else if (category == FEE_INT_CHARGE)
+            sumFeeIntCharge += get<1>(myTupleVec[i]);
+        else if (category == GAS_AUTOMOTIVE)
+            sumGasAutomotive += get<1>(myTupleVec[i]);
+        else if (category == HEALTHCARE)
+            sumHealthcare += get<1>(myTupleVec[i]);
+        else if (category == MERCHANDISE)
+            sumMerchandise += get<1>(myTupleVec[i]);
+        else if (category == OTHER)
+            sumOther += get<1>(myTupleVec[i]);
+        else if (category == OTHER_SERVICES)
+            sumOtherServices += get<1>(myTupleVec[i]);
+        else if (category == OTHER_TRAVEL)
+            sumOtherTravel += get<1>(myTupleVec[i]);
+        else if (category == PAYMENT_CREDIT)
+            sumPaymentCredit += get<1>(myTupleVec[i]);
+        else if (category == UTILITIES)
+            sumUtilities += get<1>(myTupleVec[i]);
+        else
+            sumInvalid += get<1>(myTupleVec[i]);
+    }
+
+    cout << "Debits by Category" << '\n'
+        << "----------------------------" << '\n'
+        << endl;
+
+    cout << "Airfare: \t\t$" << sumAirfare << '\n'
+        << "Car Rental: \t\t$" << sumCarRental << '\n'
+        << "Dining: \t\t$" << sumDining << '\n'
+        << "Entertainment: \t\t$" << sumEntertainment << '\n'
+        << "Fee/Interest Charge: \t$" << sumFeeIntCharge << '\n'
+        << "Gas/Automotive: \t$" << sumGasAutomotive << '\n'
+        << "Healthcare: \t\t$" << sumHealthcare << '\n'
+        << "Merchandise: \t\t$" << sumMerchandise << '\n'
+        << "Other: \t\t\t$" << sumOther << '\n'
+        << "Other Services: \t$" << sumOtherServices << '\n'
+        << "Other Travel: \t\t$" << sumOtherTravel << '\n'
+        << "Payment/Credit: \t$" << sumPaymentCredit << '\n'
+        << "Utilities: \t\t$" << sumUtilities << '\n'
+        << "Invalid: \t\t$" << sumInvalid << '\n'
+        << '\n' << "Total Spent: \t\t$" << sumAirfare + sumCarRental + sumDining + sumEntertainment + sumFeeIntCharge + sumGasAutomotive + sumHealthcare
+        + sumMerchandise + sumOther + sumOtherServices + sumOtherTravel + sumPaymentCredit + sumUtilities + sumInvalid << '\n'
+        << endl;
+
+    */
+
+    /*
     cout << "\nFinancial Data:\n";
     for (const auto& i : myTupleVec) {
         cout << "Category: " << get<0>(i) << '\n';
         cout << "Debit: $" << get<1>(i) << '\n';
         cout << endl;
     }
+    */
 
-    in.close();
+    //in.close();
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TEST ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+    CapOneCSVReader csv("CapOneData2019.csv");
+
+    cout << "Debits by Category" << '\n'
+        << "----------------------------" << '\n'
+        << endl;
+
+
+    cout << "Airfare: \t\t$"            << csv.getTotalAirfare()        << '\n'
+        << "Car Rental: \t\t$"          << csv.getTotalCarRental()      << '\n'
+        << "Dining: \t\t$"              << csv.getTotalDining()         << '\n'
+        << "Entertainment: \t\t$"       << csv.getTotalEntertainment()  << '\n'
+        << "Fee/Interest Charge: \t$"   << csv.getTotalFeeIntCharge()   << '\n'
+        << "Gas/Automotive: \t$"        << csv.getTotalGasAutomotive()  << '\n'
+        << "Healthcare: \t\t$"          << csv.getTotalHealthcare()     << '\n'
+        << "Merchandise: \t\t$"         << csv.getTotalMerchandise()    << '\n'
+        << "Other: \t\t\t$"             << csv.getTotalOther()          << '\n'
+        << "Other Services: \t$"        << csv.getTotalOtherServices()  << '\n'
+        << "Other Travel: \t\t$"        << csv.getTotalOtherTravel()    << '\n'
+        << "Payment/Credit: \t$"        << csv.getTotalPaymentCredit()  << '\n'
+        << "Utilities: \t\t$"           << csv.getTotalUtilities()      << '\n'
+
+        << '\n' << "Total Spent: \t\t$" << csv.getTotal()               << '\n'
+        << endl;
+
     std::system("pause");
 }
 
---
+++ CapOneCSVReader.cpp
@@ -0,0 +1,168 @@
+#include "CapOneCSVReader.h"
+
+CapOneCSVReader::CapOneCSVReader(std::string file_name) {
+    std::ifstream in(file_name);
+    if (!in.is_open())
+        std::cout << "ERROR: File Open" << '\n';
+
+    if (in) {
+        std::string line;
+
+        while (getline(in, line)) {
+            std::stringstream sep(line); // used for breaking words
+            std::string datum; // to store individual words
+
+            dataVec.push_back(std::vector<std::string>());
+
+            while (getline(sep, datum, ',')) {
+                dataVec.back().push_back(datum);
+                /*
+                stackoverflow.com/questions/49888882/c-determining-empty-cells-from-reading-in-data
+                place the code here, update new .csv file but with empty cells instead of zeros, and do vec.emplace_back("0")
+                */
+            }
+        }
+    }
+
+    // parse and vectorize
+    for (int i = 0; i < dataVec.size(); i++)
+        dataVecTuple.push_back(make_tuple(dataVec[i][0], stod(dataVec[i][1])));
+
+    in.close();
+}
+
+double CapOneCSVReader::getTotalAirfare() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == AIRFARE)
+            sumAirfare += std::get<1>(dataVecTuple[i]);
+    }
+    return sumAirfare;
+}
+
+double CapOneCSVReader::getTotalCarRental() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == CAR_RENTAL)
+            sumCarRental += std::get<1>(dataVecTuple[i]);
+    }
+    return sumCarRental;
+}
+
+double CapOneCSVReader::getTotalDining() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == DINING)
+            sumDining += std::get<1>(dataVecTuple[i]);
+    }
+    return sumDining;
+}
+
+double CapOneCSVReader::getTotalEntertainment() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == ENTERTAINMENT)
+            sumEntertainment += std::get<1>(dataVecTuple[i]);
+    }
+    return sumEntertainment;
+}
+
+double CapOneCSVReader::getTotalFeeIntCharge() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == FEE_INT_CHARGE)
+            sumFeeIntCharge += std::get<1>(dataVecTuple[i]);
+    }
+    return sumFeeIntCharge;
+}
+
+double CapOneCSVReader::getTotalGasAutomotive() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == GAS_AUTOMOTIVE)
+            sumGasAutomotive += std::get<1>(dataVecTuple[i]);
+    }
+    return sumGasAutomotive;
+}
+
+double CapOneCSVReader::getTotalHealthcare() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == HEALTHCARE)
+            sumHealthcare += std::get<1>(dataVecTuple[i]);
+    }
+    return sumHealthcare;
+}
+
+double CapOneCSVReader::getTotalMerchandise() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == MERCHANDISE)
+            sumMerchandise += std::get<1>(dataVecTuple[i]);
+    }
+    return sumMerchandise;
+}
+
+double CapOneCSVReader::getTotalOther() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == OTHER)
+            sumOther += std::get<1>(dataVecTuple[i]);
+    }
+    return sumOther;
+}
+
+double CapOneCSVReader::getTotalOtherServices() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == OTHER_SERVICES)
+            sumOtherServices += std::get<1>(dataVecTuple[i]);
+    }
+    return sumOtherServices;
+}
+
+double CapOneCSVReader::getTotalOtherTravel() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == OTHER_TRAVEL)
+            sumOtherTravel += std::get<1>(dataVecTuple[i]);
+    }
+    return sumOtherTravel;
+}
+
+double CapOneCSVReader::getTotalPaymentCredit() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == PAYMENT_CREDIT)
+            sumPaymentCredit += std::get<1>(dataVecTuple[i]);
+    }
+    return sumPaymentCredit;
+}
+
+double CapOneCSVReader::getTotalUtilities() {
+    for (int i = 0; i < dataVecTuple.size(); i++) {
+        std::string category = std::get<0>(dataVecTuple[i]);
+
+        if (category == UTILITIES)
+            sumUtilities += std::get<1>(dataVecTuple[i]);
+    }
+    return sumUtilities;
+}
+
+double CapOneCSVReader::getTotal() {
+    for (int i = 0; i < dataVecTuple.size(); i++)
+        sumTotal += std::get<1>(dataVecTuple[i]);
+    return sumTotal;
+}
